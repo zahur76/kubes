0cf7f325b0c03a0c3da59422ad673470274b432a
zahur <zahurmeerun@hotmail.com>
2023-03-24 14:03:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# KUBERNETES 
+
+Test project for kubernetes
+
+- Read in CSV file from AWS and the send email with information
---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+App to Ru main program
+"""
+
+def main():
+
+    print('testing')
+
+if __name__ == "__main__":
+    main()
